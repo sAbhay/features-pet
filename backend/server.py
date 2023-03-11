8e7074a60ec0f0471f9aacbee2d1e1732857a661
Abhay Singhal <abhayaaditsinghal@gmail.com>
2023-03-10 16:32:12 -0800

    removed server proxy redirecting for nginx

--- backend/server.py
+++ backend/server.py
@@ -4,9 +4,9 @@ import verify
 from werkzeug.middleware.proxy_fix import ProxyFix
 
 app = Flask(__name__)
-app.wsgi_app = ProxyFix(
-    app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1
-)
+# app.wsgi_app = ProxyFix(
+#     app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1
+# )
 
 @app.route('/v1/generate/<prompt>')
 def generate_given_prompt(prompt):
